d13b134fcec31f565217557fe6ff17b45fedae5f
Chris Johnston <christopherjohnston@protonmail.com>
2021-08-03 19:57:13 +0100

    Updates

---
+++ component---src-pages-404-js-576b7ffbd056b79f28ed.js
@@ -0,0 +1,2 @@
+(self.webpackChunkportfolio=self.webpackChunkportfolio||[]).push([[883],{9616:function(e,t,n){"use strict";n.r(t);var o=n(7294),l=n(5444),a={color:"black",padding:"96px",fontFamily:"-apple-system, Roboto, sans-serif, serif"};t.default=function(){return o.createElement("main",{style:a},o.createElement("title",null,"Not found"),o.createElement("p",null,"Page not found"),o.createElement(l.Link,{to:"/"},"Go home"),".")}}}]);
+//# sourceMappingURL=component---src-pages-404-js-576b7ffbd056b79f28ed.js.map
